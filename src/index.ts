c7ed79b2ee5b514a1d8ebd2afbf5b39649728c2b
Carver <kerenketepela@gmail.com>
2022-03-01 17:51:15 +0300

    feat:conn to metamsk

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,38 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
-const getEth = () =>{
+async function hasSigners(): Promise<boolean> {
     //@ts-ignore
-    const eth = window.ethereum
-    if(!eth){
-        throw console.error("error");
-    }
+    const metamask = window.ethereum;
+    const signers = await (metamask.request({method:'eth_accounts'}) as Promise<string[]>);
+    return signers.length > 0;
 }
 
-const hasAccount = async () =>{
-    const eth = getEth()
-    const account = await eth.request({method:"eth_accounts"}) as string[]
-    return account && account.length
+async function requestAccess(): Promise<boolean> {
+    //@ts-ignore
+    const result = (await window.ethereum.request({ method: 'eth_requestAccounts' })) as string[];
+    return result && result.length > 0;
 }
+
+async function getContract() {
+    const address = process.env.CONTRACT_ADDRESS;
+
+    if (!(await hasSigners()) && !(await requestAccess())) {
+        console.log("You are in trouble, no one wants to play");
+    }
+
+    // @ts-ignore
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const contract = new ethers.Contract(
+    "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+        [
+            "function hello() public pure returns(string memory)",
+        ], // abi
+        provider
+    );
+
+    console.log("We have done it, time to call");
+    console.log(await contract.hello());
+}
+
+
+getContract();
